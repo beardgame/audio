e0cd6fae2385ce699d8590ea7567a44b1cd92650
Tom <tom@tomrijnbeek.nl>
2016-10-16 13:25:32 +0100

    Add AudioContext.

---
+++ src/TomRijnbeek.Audio/Core/AudioConfig.cs
@@ -0,0 +1,79 @@
+﻿namespace TomRijnbeek.Audio {
+    /// <summary>
+    /// Immutable container class for the audio configuration.
+    /// </summary>
+    public class AudioConfig {
+        /// <summary>
+        /// The maximum number of sources that can be active simulataneously.
+        /// </summary>
+        public int MaxSources { get; private set; }
+
+        private AudioConfig() { }
+
+        #region Defaults
+        /// <summary>
+        /// The default configuration object.
+        /// </summary>
+        public static readonly AudioConfig Default;
+
+        static AudioConfig() {
+            Default = new AudioConfig();
+            Default.MaxSources = 32;
+        }
+        #endregion
+
+        #region Builder
+        /// <summary>
+        /// Returns a new builder based on the default configuration.
+        /// </summary>
+        /// <returns>The builder.</returns>
+        public static Builder newBuilder() {
+            return Builder.From(Default);
+        }
+
+        /// <summary>
+        /// Builder object that can be used to customise the configuration.
+        /// </summary>
+        public class Builder {
+            /// <summary>
+            /// Creates a new builder based on an existing audio configuration.
+            /// </summary>
+            /// <param name="reference">Reference.</param>
+            public static Builder From(AudioConfig reference) {
+                var builder = new Builder();
+                copyConfig(reference, builder.workInstance);
+                return builder;
+            }
+
+            private AudioConfig workInstance;
+
+            private Builder() {
+                workInstance = new AudioConfig();
+            }
+
+            /// <summary>
+            /// Sets the maximum number of sources.
+            /// </summary>
+            /// <returns>The builder.</returns>
+            /// <param name="maxSources">maximum number of sources.</param>
+            public Builder SetMaxSources(int maxSources) {
+                workInstance.MaxSources = maxSources;
+                return this;
+            }
+
+            /// <summary>
+            /// Build this instance.
+            /// </summary>
+            public AudioConfig Build() {
+                var returnInstance = new AudioConfig();
+                copyConfig(workInstance, returnInstance);
+                return returnInstance;
+            }
+        }
+        #endregion
+
+        private static void copyConfig(AudioConfig from, AudioConfig to) {
+            to.MaxSources = from.MaxSources;
+        }
+    }
+}
---
+++ src/TomRijnbeek.Audio/Core/AudioContext.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+using ALContext = OpenTK.Audio.AudioContext;
+
+namespace TomRijnbeek.Audio {
+    /// <summary>
+    /// Main context for using any audio related code.
+    /// Should be instantiated before using any of the library's code.
+    /// </summary>
+    public class AudioContext : IDisposable {
+        #region Singleton
+        private static AudioContext instance;
+
+        /// <summary>
+        /// Gets the sole AudioContext instance.
+        /// Returns an exception if 
+        /// </summary>
+        /// <value>The instance.</value>
+        public static AudioContext Instance { 
+            get {
+                if (instance == null) {
+                    throw new NullReferenceException(
+                        "The AudioContext must be initialised before any audio code can be executed.");
+                }
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the audio context with the default configuration.
+        /// </summary>
+        public static void Initialize() {
+            Initialize(AudioConfig.Default);
+        }
+
+        /// <summary>
+        /// Initializes the audio context with the specified configuration.
+        /// </summary>
+        /// <param name="config">Config.</param>
+        public static void Initialize(AudioConfig config) {
+            if (instance != null) {
+                throw new InvalidOperationException("Only one audio context can be instantiated.");
+            }
+
+            instance = new AudioContext(config);
+        }
+        #endregion
+
+        private readonly AudioConfig config;
+        private readonly ALContext ctx;
+
+        /// <summary>
+        /// The configuration used by this context.
+        /// </summary>
+        /// <value>The config.</value>
+        public AudioConfig Config {
+            get {
+                return this.config;
+            }
+        }
+
+        private AudioContext(AudioConfig config) {
+            this.config = config;
+            this.ctx = new ALContext();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        private void dispose(bool disposing) {
+            if (!disposedValue) {
+                if (disposing) {
+                    // Dispose managed state (managed objects).
+                }
+
+                this.ctx.Dispose();
+
+                disposedValue = true;
+                instance = null;
+            }
+        }
+
+        /// <summary>
+        /// Releases unmanaged resources and performs other cleanup operations before the
+        /// <see cref="T:TomRijnbeek.Audio.AudioContext"/> is reclaimed by garbage collection.
+        /// </summary>
+        ~AudioContext() {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            dispose(false);
+        }
+
+        /// <summary>
+        /// Releases all resource used by the <see cref="T:TomRijnbeek.Audio.AudioContext"/> object.
+        /// </summary>
+        /// <remarks>Call <see cref="Dispose"/> when you are finished using the <see cref="T:TomRijnbeek.Audio.AudioContext"/>.
+        /// The <see cref="Dispose"/> method leaves the <see cref="T:TomRijnbeek.Audio.AudioContext"/> in an unusable
+        /// state. After calling <see cref="Dispose"/>, you must release all references to the
+        /// <see cref="T:TomRijnbeek.Audio.AudioContext"/> so the garbage collector can reclaim the memory that the
+        /// <see cref="T:TomRijnbeek.Audio.AudioContext"/> was occupying.</remarks>
+        public void Dispose() {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
