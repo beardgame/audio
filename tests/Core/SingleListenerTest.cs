7eeece22ce24518dbb2d899c693718608415defb
Tom Rijnbeek <tom@tomrijnbeek.nl>
2017-02-03 23:16:11 +0000

    Improve wrapping for AL interface (#10)
    * Add AudioContext.
    
    * Moved ALListener to use audio wrapper.
    
    * Copy pasta
    
    * Refactored AL wrapper into services.
    
    * Fix tests.
    
    * Add rider files to gitignore.
    
    * Fix comment.
    
    * Test improvements.
    
    * Experiment with builds.
    
    * Do not call base.
    
    * Mark mocked methods as virtual,:

--- tests/Core/SingleListenerTest.cs
+++ tests/Core/SingleListenerTest.cs
@@ -2,27 +2,27 @@
 using System;
 using Xunit;
 
-// TODO: these tests are disabled
 namespace TomRijnbeek.Audio.Tests {
     public class SingleListenerTest {
         private readonly Mock<ListenerService> svcMock;
 
         public SingleListenerTest() {
             AudioContext.InitializeForTest();
-            this.svcMock = new Mock<ListenerService>();
-            ListenerService.SetTestInstance(this.svcMock.Object);
+            svcMock = new Mock<ListenerService>() { CallBase = false };
+            ListenerService.SetTestInstance(svcMock.Object);
         }
 
-        //[Fact]
-        //public void RegistersListener() {
-        //    var listener = new SingleListener();
-        //    Assert.Equal(ALListener.Get(), listener);
-        //}
+        [Fact]
+        public void RegistersListener() {
+            ALListener.Set(null);
+            var listener = new SingleListener();
+            Assert.Equal(ALListener.Get(), listener);
+        }
 
-        //[Fact]
-        //public void ForbidsMultipleListeners() {
-        //    ALListener.Set(new Mock<IListener>().Object);
-        //    Assert.Throws<InvalidOperationException>(() => new SingleListener());
-        //}
+        [Fact]
+        public void ForbidsMultipleListeners() {
+            ALListener.Set(new Mock<IListener>().Object);
+            Assert.Throws<InvalidOperationException>(() => new SingleListener());
+        }
     }
 }
