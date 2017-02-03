bd8a9031eec8a6a771c9f00e7f4f0cd9a3994853
Tom <tom@tomrijnbeek.nl>
2017-02-03 23:07:34 +0000

    Mark mocked methods as virtual,:

--- src/TomRijnbeek.Audio/Core/services/ListenerService.cs
+++ src/TomRijnbeek.Audio/Core/services/ListenerService.cs
@@ -3,34 +3,34 @@ using OpenTK.Audio.OpenAL;
 
 namespace TomRijnbeek.Audio {
     class ListenerService : AudioService<ListenerService> {
-        public float GetProperty(ALListenerf property) {
+        public virtual float GetProperty(ALListenerf property) {
             float value;
             AL.GetListener(property, out value);
             ctx.CheckErrors();
             return value;
         }
 
-        public Vector3 GetProperty(ALListener3f property) {
+        public virtual Vector3 GetProperty(ALListener3f property) {
             Vector3 value;
             AL.GetListener(property, out value);
             ctx.CheckErrors();
             return value;
         }
 
-        public void GetProperty(ALListenerfv property, out Vector3 at, out Vector3 up) {
+        public virtual void GetProperty(ALListenerfv property, out Vector3 at, out Vector3 up) {
             AL.GetListener(property, out at, out up);
             ctx.CheckErrors();
         }
 
-        public void SetProperty(ALListenerf property, float value) {
+        public virtual void SetProperty(ALListenerf property, float value) {
             ctx.Call(AL.Listener, property, value);
         }
 
-        public void SetProperty(ALListener3f property, Vector3 value) {
+        public virtual void SetProperty(ALListener3f property, Vector3 value) {
             ctx.Call(() => AL.Listener(property, ref value));
         }
 
-        public void SetProperty(ALListenerfv property, Vector3 at, Vector3 up) {
+        public virtual void SetProperty(ALListenerfv property, Vector3 at, Vector3 up) {
             ctx.Call(() => AL.Listener(property, ref at, ref up));
         }
     }
