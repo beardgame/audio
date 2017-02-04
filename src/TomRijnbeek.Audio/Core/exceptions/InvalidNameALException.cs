798fb06372507bf2c8760511c2825ae7c90f3a94
Tom Rijnbeek <tom@tomrijnbeek.nl>
2017-02-04 13:09:22 +0000

    Implement error handling (see #4) (#12)
    * Add exception types.
    
    * Add missing newlines.

---
+++ src/TomRijnbeek.Audio/Core/exceptions/InvalidNameALException.cs
@@ -0,0 +1,14 @@
+﻿using OpenTK.Audio.OpenAL;
+
+namespace TomRijnbeek.Audio {
+    /// <summary>
+    /// Thrown when a specified name or handle is invalid (e.g. does not exist).
+    /// </summary>
+    public sealed class InvalidNameALException : ALException {
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        /// <param name="message">Exception message.</param>
+        public InvalidNameALException(string message) : base(ALError.InvalidName, message) { }
+    }
+}
