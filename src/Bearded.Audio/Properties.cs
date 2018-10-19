6bcac46036af535618eb85647f6099d68f942fc6
Tom Rijnbeek <828553+tomrijnbeek@users.noreply.github.com>
2018-10-19 21:11:32 +0100

    :arrow_up_small: Use new package structure (#24)
    * :arrow_up_small: Use new package structure
    
    * :file_folder: Improve folder structure

---
+++ src/Bearded.Audio/Properties.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+
+// Expose internal properties to unit tests project.
+[assembly: InternalsVisibleTo("Bearded.Audio.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
