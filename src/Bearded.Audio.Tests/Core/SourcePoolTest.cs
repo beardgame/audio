614f05bd1b6113fafc4f150281e2ee5f882f10b2
Tom Rijnbeek <tom@tomrijnbeek.nl>
2019-04-12 23:26:25 +0100

    Implement a basic SourcePool class (#31)
    * :sparkle: Add SourcePool class
    
    * :bug: Add some additional checks and code
    
    * :white_check_mark: Implement tests
    
    * Fix bug in finalizer when constructor doesn't complete
    
    * :lipstick:
    
    * :pencil: Use separate capacity for source pool + other fixes
    
    * :lipstick:
    
    * :pencil: Remove GetSource and other review changes
    
    * :pencil: Split disposal of managed & unmanaged resources
    
    * :fire_engine: Improve disposing behaviour

---
+++ src/Bearded.Audio.Tests/Core/SourcePoolTest.cs
@@ -0,0 +1,245 @@
+using System;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace Bearded.Audio.Tests {
+    public sealed class SourcePoolTest {
+        private readonly Mock<ISourceService> svcMock;
+
+        public SourcePoolTest() {
+            svcMock = new Mock<ISourceService>();
+            SourceService.SetTestInstance(svcMock.Object);
+            BufferService.SetTestInstance(new Mock<IBufferService>().Object);
+        }
+
+        [Fact]
+        public void CreateInstanceAndAllocateSources_CreatesInstanceWithCorrectCapacity() {
+            const int numSources = 10;
+
+            var instance = SourcePool.CreateInstanceAndAllocateSources(numSources);
+
+            instance.Capacity.Should().Be(numSources);
+        }
+
+        [Fact]
+        public void CreateInstanceAndAllocateSources_ThrowsExceptionForZeroCapacity() {
+            Action createInstanceAndAllocateSources = () => SourcePool.CreateInstanceAndAllocateSources(0);
+
+            createInstanceAndAllocateSources.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void CreateInstanceAndAllocateSources_ThrowsExceptionForNegativeCapacity() {
+            Action createInstanceAndAllocateSources = () => SourcePool.CreateInstanceAndAllocateSources(-42);
+
+            createInstanceAndAllocateSources.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void CreateInstanceAndAllocateSources_ImmediatelyAllocatesSources() {
+            const int numSources = 7;
+
+            SourcePool.CreateInstanceAndAllocateSources(numSources);
+
+            svcMock.Verify(svc => svc.Generate(), Times.Exactly(numSources));
+        }
+
+        [Fact]
+        public void CreateInstance_CreatesInstanceWithCorrectCapacity() {
+            const int numSources = 19;
+
+            var instance = SourcePool.CreateInstance(numSources);
+
+            instance.Capacity.Should().Be(numSources);
+        }
+
+        [Fact]
+        public void CreateInstance_ThrowsExceptionForZeroCapacity() {
+            Action createInstance = () => SourcePool.CreateInstance(0);
+
+            createInstance.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void CreateInstance_ThrowsExceptionForNegativeCapacity() {
+            Action createInstance = () => SourcePool.CreateInstance(-32);
+
+            createInstance.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void CreateInstanceAndAllocateSources_DoesNotImmediatelyAllocateSources() {
+            const int numSources = 4;
+
+            SourcePool.CreateInstance(numSources);
+
+            svcMock.Verify(svc => svc.Generate(), Times.Never);
+        }
+
+        [Fact]
+        public void TryGetSource_ReturnsTrueIfASourceIsAvailable() {
+            var instance = SourcePool.CreateInstance(1);
+
+            var result = instance.TryGetSource(out _);
+
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void TryGetSource_ReturnsASource() {
+            var instance = SourcePool.CreateInstance(1);
+
+            instance.TryGetSource(out var source);
+
+            source.Should().BeAssignableTo<Source>().And.NotBeNull();
+        }
+
+        [Fact]
+        public void TryGetSource_AllocatesNewSourceIfNoneAllocated() {
+            var instance = SourcePool.CreateInstance(1);
+
+            svcMock.Invocations.Clear();
+
+            instance.TryGetSource(out _);
+
+            svcMock.Verify(svc => svc.Generate(), Times.Once);
+        }
+
+        [Fact]
+        public void TryGetSource_ReturnsFalseIfThereIsNoAvailableSource() {
+            var instance = SourcePool.CreateInstance(1);
+            instance.TryGetSource(out _);
+
+            var result = instance.TryGetSource(out _);
+
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void TryGetSource_ShouldUseUpCapacity() {
+            var instance = SourcePool.CreateInstance(1);
+
+            instance.TryGetSource(out _);
+
+            instance.HasAvailableSource.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ReclaimSource_ThrowsOnNull() {
+            var instance = SourcePool.CreateInstance(1);
+
+            Action reclaimSource = () => instance.ReclaimSource(null);
+
+            reclaimSource.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void ReclaimSource_ThrowsForDisposedSource() {
+            var instance = SourcePool.CreateInstance(1);
+            instance.TryGetSource(out var source);
+            source.Dispose();
+
+            Action reclaimSource = () => instance.ReclaimSource(source);
+
+            reclaimSource.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void ReclaimSource_ThrowsForSourceNotInSourcePool() {
+            var instance = SourcePool.CreateInstance(1);
+            var source = new Source();
+
+            Action reclaimSource = () => instance.ReclaimSource(source);
+
+            reclaimSource.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void ReclaimSource_ThrowsIfSourceIsStillPlaying() {
+            var instance = SourcePool.CreateInstance(1);
+            instance.TryGetSource(out var source);
+            source.Looping = true;
+
+            Action reclaimSource = () => instance.ReclaimSource(source);
+
+            reclaimSource.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void ReclaimSource_MakesSourceAvailableForReuse() {
+            var instance = SourcePool.CreateInstance(1);
+            instance.TryGetSource(out var source);
+
+            instance.ReclaimSource(source);
+
+            instance.HasAvailableSource.Should().BeTrue();
+            instance.TryGetSource(out var availableSource);
+            availableSource.Should().Be(source);
+        }
+
+        [Fact]
+        public void ReclaimSource_ShouldRewindSource() {
+            var instance = SourcePool.CreateInstance(1);
+            instance.TryGetSource(out var source);
+            source.Play();
+
+            svcMock.Invocations.Clear();
+
+            instance.ReclaimSource(source);
+
+            svcMock.Verify(svc => svc.Rewind(source), Times.Once);
+        }
+
+        [Fact]
+        public void ReclaimAllFinishedSources_ReclaimsOnlyFinishedSources() {
+            var instance = SourcePool.CreateInstance(2);
+            instance.TryGetSource(out var source1);
+            instance.TryGetSource(out var source2);
+            source1.Looping = true;
+
+            instance.ReclaimAllFinishedSources();
+
+            instance.HasAvailableSource.Should().BeTrue();
+            instance.TryGetSource(out var availableSource);
+            availableSource.Should().Be(source2);
+            instance.HasAvailableSource.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ReclaimAllFinishedSources_ThrowsIfAnySourceDisposed() {
+            var instance = SourcePool.CreateInstance(2);
+            instance.TryGetSource(out var source1);
+            instance.TryGetSource(out _);
+            source1.Dispose();
+
+            Action reclaimAllFinishedSources = () => instance.ReclaimAllFinishedSources();
+
+            reclaimAllFinishedSources.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void ReclaimAllFinishedSources_RewindsReclaimedSource() {
+            var instance = SourcePool.CreateInstance(2);
+            instance.TryGetSource(out var source1);
+            instance.TryGetSource(out var source2);
+            source1.Looping = true;
+
+            svcMock.Invocations.Clear();
+
+            instance.ReclaimAllFinishedSources();
+
+            svcMock.Verify(svc => svc.Rewind(source2), Times.Once);
+        }
+
+        [Fact]
+        public void Dispose_DisposesAllSources() {
+            var instance = SourcePool.CreateInstance(1);
+            instance.TryGetSource(out var source);
+
+            instance.Dispose();
+
+            source.Disposed.Should().BeTrue();
+        }
+    }
+}
---
+++ src/Bearded.Audio/Core/SourcePool.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Bearded.Audio {
+    /// <summary>
+    /// Represents a pool of sources that can be reused to prevent continuous allocation and deallocation, and stops
+    /// the program from using more sources than allowed by the sound driver.
+    /// </summary>
+    public sealed class SourcePool : IDisposable {
+        private readonly List<Source> sources;
+        private readonly Queue<Source> availableSources;
+        private bool disposed;
+
+        private bool hasReachedCapacity => sources.Count == Capacity;
+
+        private bool hasAvailableAllocatedSource => availableSources.Count > 0;
+
+        /// <summary>
+        /// The number of sources this source pool manages.
+        /// </summary>
+        public int Capacity { get; }
+
+        /// <summary>
+        /// Whether there is currently a source available for playing.
+        /// </summary>
+        public bool HasAvailableSource => hasAvailableAllocatedSource || !hasReachedCapacity;
+
+        /// <summary>
+        /// Creates a new source pool with the specified number of sources, and eagerly allocates the sources to this
+        /// pool.
+        /// </summary>
+        public static SourcePool CreateInstanceAndAllocateSources(int numSources) {
+            var sourcePool = CreateInstance(numSources);
+
+            for (var i = 0; i < numSources; i++) {
+                sourcePool.allocateNewSource();
+            }
+
+            return sourcePool;
+        }
+
+        /// <summary>
+        /// Creates a new source pool.
+        /// </summary>
+        public static SourcePool CreateInstance(int numSources) => new SourcePool(numSources);
+
+        private SourcePool(int numSources) {
+            if (numSources <= 0) {
+                throw new ArgumentException(
+                    "Cannot create a source pool with a non-positive number of sources", nameof(numSources));
+            }
+
+            Capacity = numSources;
+            sources = new List<Source>(Capacity);
+            availableSources = new Queue<Source>(Capacity);
+        }
+
+        /// <summary>
+        /// Attempts to get an available source. Returns false if no source could be assigned.
+        /// </summary>
+        public bool TryGetSource(out Source source) {
+            checkNotDisposed();
+            ensureSourceAvailableIfPossible();
+
+            if (hasAvailableAllocatedSource) {
+                source = availableSources.Dequeue();
+                return true;
+            }
+
+            source = null;
+            return false;
+        }
+
+        private void ensureSourceAvailableIfPossible() {
+            if (hasAvailableAllocatedSource) {
+                return;
+            }
+
+            if (!hasReachedCapacity) {
+                allocateNewSource();
+            }
+        }
+
+        private void allocateNewSource() {
+            Debug.Assert(!hasReachedCapacity, "Should not call allocateNewSource when pool has reached capacity.");
+
+            var source = new Source();
+            sources.Add(source);
+            availableSources.Enqueue(source);
+        }
+
+        /// <summary>
+        /// Reclaims all sources that are not currently playing any sound.
+        /// </summary>
+        public void ReclaimAllFinishedSources() {
+            checkNotDisposed();
+            foreach (var source in sources.Where(s => s.FinishedPlaying)) {
+                reclaimSource(source);
+            }
+        }
+
+        /// <summary>
+        /// Reclaims a source by adding it back to the available sources pool.
+        ///
+        /// <para>Sources to be reclaimed should no longer be playing and should not be disposed.</para>
+        /// </summary>
+        public void ReclaimSource(Source source) {
+            checkNotDisposed();
+            if (source == null) {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if (!sources.Contains(source)) {
+                throw new ArgumentException(
+                    "Cannot reclaim a source that is not part of this source pool", nameof(source));
+            }
+            if (!source.FinishedPlaying) {
+                throw new ArgumentException("Cannot reclaim a source that has not finished playing", nameof(source));
+            }
+
+            reclaimSource(source);
+        }
+
+        private void reclaimSource(Source source) {
+            if (source.Disposed) {
+                throw new ArgumentException("Cannot reclaim a disposed source.", nameof(source));
+            }
+
+            resetSource(source);
+            availableSources.Enqueue(source);
+        }
+
+        private static void resetSource(Source source) {
+            source.DequeueProcessedBuffers();
+            source.Rewind();
+        }
+
+        public void Dispose() {
+            if (disposed) return;
+
+            foreach (var source in sources) {
+                source.Dispose();
+            }
+
+            sources.Clear();
+            availableSources.Clear();
+
+            disposed = true;
+        }
+
+        private void checkNotDisposed() {
+            if (disposed) {
+                throw new InvalidOperationException("Cannot use a SourcePool after disposing it.");
+            }
+        }
+    }
+}
