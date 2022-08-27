34088e5ed5467aeac3789432325008a12ed381d5
Tom Rijnbeek <tom@tomrijnbeek.me>
2022-08-27 15:31:33 +0200

    💄

--- src/Bearded.Audio/Core/SoundBufferData.cs
+++ src/Bearded.Audio/Core/SoundBufferData.cs
@@ -21,7 +21,7 @@ public sealed class SoundBufferData
     /// </summary>
     /// <param name="buffers">The content of the buffers.</param>
     /// <param name="format">The format the buffers are in.</param>
-    /// <param name="sampleRate">The samplerate of the buffers.</param>
+    /// <param name="sampleRate">The sample rate of the buffers.</param>
     public SoundBufferData(IList<short[]> buffers, ALFormat format, int sampleRate)
     {
         Buffers = buffers;
@@ -30,7 +30,7 @@ public sealed class SoundBufferData
     }
 
     /// <summary>
-    /// Extracts the bufferdata from an uncompressed wave-file.
+    /// Extracts the buffer data from an uncompressed wave-file.
     /// </summary>
     /// <param name="file">The file to load the data from.</param>
     /// <returns>A SoundBufferData object containing the data from the specified file.</returns>
@@ -40,76 +40,75 @@ public sealed class SoundBufferData
     }
 
     /// <summary>
-    /// Extracts the bufferdata from an uncompressed wave-file.
+    /// Extracts the buffer data from an uncompressed wave-file.
     /// </summary>
     /// <param name="file">The file to load the data from.</param>
     /// <returns>A SoundBufferData object containing the data from the specified file.</returns>
     public static SoundBufferData FromWav(Stream file)
     {
-        using (var reader = new BinaryReader(file))
+        using var reader = new BinaryReader(file);
+
+        // RIFF header
+        var signature = new string(reader.ReadChars(4));
+        if (signature != "RIFF")
         {
-            // RIFF header
-            var signature = new string(reader.ReadChars(4));
-            if (signature != "RIFF")
-            {
-                throw new NotSupportedException("Specified stream is not a wave file.");
-            }
-
-            reader.ReadInt32(); // riffChunkSize
-
-            var format = new string(reader.ReadChars(4));
-            if (format != "WAVE")
-            {
-                throw new NotSupportedException("Specified stream is not a wave file.");
-            }
-
-            // WAVE header
-            var formatSignature = new string(reader.ReadChars(4));
-            if (formatSignature != "fmt ")
-            {
-                throw new NotSupportedException("Specified wave file is not supported.");
-            }
-
-            var formatChunkSize = reader.ReadInt32();
-            reader.ReadInt16(); // audioFormat
-            int numChannels = reader.ReadInt16();
-            var sampleRate = reader.ReadInt32();
-            reader.ReadInt32(); // byteRate
-            reader.ReadInt16(); // blockAlign
-            int bitsPerSample = reader.ReadInt16();
-
-            if (formatChunkSize > 16)
-            {
-                reader.ReadBytes(formatChunkSize - 16);
-            }
-
-            var dataSignature = new string(reader.ReadChars(4));
-
-            if (dataSignature != "data")
-            {
-                throw new NotSupportedException("Only uncompressed wave files are supported.");
-            }
-
-            reader.ReadInt32(); // dataChunkSize
-
-            var alFormat = getSoundFormat(numChannels, bitsPerSample);
-
-            var data = reader.ReadBytes((int) reader.BaseStream.Length);
-            var buffers = new List<short[]>();
-            int count;
-            var i = 0;
-            const int bufferSize = 16384;
-
-            while ((count = (Math.Min(data.Length, (i + 1) * bufferSize * 2) - i * bufferSize * 2) / 2) > 0)
-            {
-                var buffer = new short[bufferSize];
-                convertBuffer(data, buffer, count, i * bufferSize * 2);
-                buffers.Add(buffer);
-                i++;
-            }
-
-            return new SoundBufferData(buffers, alFormat, sampleRate);
+            throw new NotSupportedException("Specified stream is not a wave file.");
         }
+
+        reader.ReadInt32(); // riffChunkSize
+
+        var format = new string(reader.ReadChars(4));
+        if (format != "WAVE")
+        {
+            throw new NotSupportedException("Specified stream is not a wave file.");
+        }
+
+        // WAVE header
+        var formatSignature = new string(reader.ReadChars(4));
+        if (formatSignature != "fmt ")
+        {
+            throw new NotSupportedException("Specified wave file is not supported.");
+        }
+
+        var formatChunkSize = reader.ReadInt32();
+        reader.ReadInt16(); // audioFormat
+        int numChannels = reader.ReadInt16();
+        var sampleRate = reader.ReadInt32();
+        reader.ReadInt32(); // byteRate
+        reader.ReadInt16(); // blockAlign
+        int bitsPerSample = reader.ReadInt16();
+
+        if (formatChunkSize > 16)
+        {
+            reader.ReadBytes(formatChunkSize - 16);
+        }
+
+        var dataSignature = new string(reader.ReadChars(4));
+
+        if (dataSignature != "data")
+        {
+            throw new NotSupportedException("Only uncompressed wave files are supported.");
+        }
+
+        reader.ReadInt32(); // dataChunkSize
+
+        var alFormat = getSoundFormat(numChannels, bitsPerSample);
+
+        var data = reader.ReadBytes((int) reader.BaseStream.Length);
+        var buffers = new List<short[]>();
+        int count;
+        var i = 0;
+        const int bufferSize = 16384;
+
+        while ((count = (Math.Min(data.Length, (i + 1) * bufferSize * 2) - i * bufferSize * 2) / 2) > 0)
+        {
+            var buffer = new short[bufferSize];
+            convertBuffer(data, buffer, count, i * bufferSize * 2);
+            buffers.Add(buffer);
+            i++;
+        }
+
+        return new SoundBufferData(buffers, alFormat, sampleRate);
     }
 
     private static void convertBuffer(byte[] inBuffer, short[] outBuffer, int length, int inOffset = 0)
@@ -120,13 +119,12 @@ public sealed class SoundBufferData
         }
     }
 
-    private static ALFormat getSoundFormat(int channels, int bits)
+    private static ALFormat getSoundFormat(int channels, int bits) => (channels, bits) switch
     {
-        switch (channels)
-        {
-            case 1: return bits == 8 ? ALFormat.Mono8 : ALFormat.Mono16;
-            case 2: return bits == 8 ? ALFormat.Stereo8 : ALFormat.Stereo16;
-            default: throw new NotSupportedException("The specified sound format is not supported.");
-        }
-    }
+        (1, 8) => ALFormat.Mono8,
+        (1, 16) => ALFormat.Mono16,
+        (2, 8) => ALFormat.Stereo8,
+        (2, 16) => ALFormat.Stereo16,
+        _ => throw new NotSupportedException("The specified sound format is not supported.")
+    };
 }
