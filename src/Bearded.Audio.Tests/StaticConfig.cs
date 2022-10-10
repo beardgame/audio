9c131e66cf1925c98f28e831fbbb28b292c31949
Tom Rijnbeek <tom@tomrijnbeek.me>
2022-10-10 17:52:59 +0200

    ✅ Add snapshot tests for wav file parsing

---
+++ src/Bearded.Audio.Tests/StaticConfig.cs
@@ -0,0 +1,16 @@
+﻿using VerifyTests;
+
+namespace Bearded.Audio.Tests;
+
+public static class StaticConfig
+{
+    public static VerifySettings DefaultVerifySettings
+    {
+        get
+        {
+            var settings = new VerifySettings();
+            settings.UseDirectory("goldens");
+            return settings;
+        }
+    }
+}
