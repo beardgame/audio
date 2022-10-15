011cfb84d852311d82d390dada15f19509c5a6e5
Tom Rijnbeek <tom@tomrijnbeek.me>
2022-10-15 21:02:24 +0200

    💡 Add missing summary documentation

--- src/Bearded.Audio/Core/ALListener.cs
+++ src/Bearded.Audio/Core/ALListener.cs
@@ -4,16 +4,29 @@ using OpenTK.Mathematics;
 
 namespace Bearded.Audio;
 
+/// <summary>
+/// Represents the single static listener in the OpenAL space.
+/// This instance can be given an instance of the <see cref="IListener"/> type, and it will automatically synchronize
+/// properties. This allows swapping out multiple listeners, or have an object be passed around to update properties.
+/// </summary>
 [PublicAPI]
 public static class ALListener
 {
     private static IListener? currentListener;
 
+    /// <summary>
+    /// Retrieves the <see cref="IListener"/> currently being synchronized with the AL Listener.
+    /// </summary>
     public static IListener? Get()
     {
         return currentListener;
     }
 
+    /// <summary>
+    /// Sets a <see cref="IListener"/> instance with which the properties in the OpenAL space will automatically be
+    /// synchronized.
+    /// </summary>
+    /// <param name="listener">The listener from which the properties should be synchronized.</param>
     public static void Set(IListener? listener)
     {
         if (currentListener != null)
@@ -22,11 +35,13 @@ public static class ALListener
         }
 
         currentListener = listener;
-        if (listener != null)
+        if (listener == null)
         {
-            updateProperties(listener);
-            listener.ListenerUpdated += updateProperties;
+            return;
         }
+
+        updateProperties(listener);
+        listener.ListenerUpdated += updateProperties;
     }
 
     private static void updateProperties(IListener? reference)
@@ -62,6 +77,13 @@ public static class ALListener
         }
     }
 
+    /// <summary>
+    /// The position of the virtual listener in the OpenAL space used to calculate the relative position of sound
+    /// sources to determine channel mixing.
+    /// </summary>
+    /// <remarks>
+    /// The positions of the listener and sound source is ignored for audio playback with multiple channels.
+    /// </remarks>
     public static Vector3 Position
     {
         get
@@ -77,6 +99,13 @@ public static class ALListener
         }
     }
 
+    /// <summary>
+    /// The velocity of the virtual listener in the OpenAL space used to calculate the relative velocity of sound
+    /// sources to simulate the Doppler effect.
+    /// </summary>
+    /// <remarks>
+    /// The velocities of the listener and sound source is ignored for audio playback with multiple channels.
+    /// </remarks>
     public static Vector3 Velocity
     {
         get
@@ -92,6 +121,10 @@ public static class ALListener
         }
     }
 
+    /// <summary>
+    /// The factor by which the audio amplitudes is multiplied when played back. This allows control over the global
+    /// audio volume.
+    /// </summary>
     public static float Gain
     {
         get
@@ -103,6 +136,13 @@ public static class ALListener
         set => AudioContext.Instance.Call(AL.Listener, ALListenerf.Gain, value);
     }
 
+    /// <summary>
+    /// The forward direction of the virtual listener in the OpenAL space used to calculate the relative position of
+    /// sound sources to determine channel mixing.
+    /// </summary>
+    /// <remarks>
+    /// The orientations of the listener and sound source is ignored for audio playback with multiple channels.
+    /// </remarks>
     public static Vector3 At
     {
         get
@@ -119,6 +159,13 @@ public static class ALListener
         }
     }
 
+    /// <summary>
+    /// The upward direction of the virtual listener in the OpenAL space used to calculate the relative position of
+    /// sound sources to determine channel mixing.
+    /// </summary>
+    /// <remarks>
+    /// The orientations of the listener and sound source is ignored for audio playback with multiple channels.
+    /// </remarks>
     public static Vector3 Up
     {
         get
